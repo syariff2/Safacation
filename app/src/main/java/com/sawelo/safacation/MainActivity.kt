15f902bbc9f69ef93717134b086946a550dc1af7
Chompions <chompionsuratman@gmail.com>
2021-12-05 16:16:30 +0800

    Menambahkan bottom navigation

--- app/src/main/java/com/sawelo/safacation/MainActivity.kt
+++ app/src/main/java/com/sawelo/safacation/MainActivity.kt
@@ -2,10 +2,37 @@ package com.sawelo.safacation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sawelo.safacation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            loadFragment(LocationFragment())
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.location_tab -> loadFragment(LocationFragment())
+                R.id.health_tab -> loadFragment(HealthFragment())
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragment_container_view, fragment)
+        }
     }
 }
