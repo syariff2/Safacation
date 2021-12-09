a80e7a7e67d24a7f5f8b92e756dcaa81b50e0cfc
syariff2 <79697004+syariff2@users.noreply.github.com>
2021-12-09 20:21:44 +0700

    Mengisi data AdapterSafa.kt

--- app/src/main/java/com/sawelo/safacation/AdapterSafa.kt
+++ app/src/main/java/com/sawelo/safacation/AdapterSafa.kt
@@ -1,23 +1,48 @@
 package com.sawelo.safacation
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
-class AdapterSafa(private val listSafa: ArrayList<DataSafa>) : RecyclerView.Adapter<AdapterSafa.ListViewHolder>() {
-    class ListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class AdapterSafa(private val listSafa : ArrayList<DataSafa>) : RecyclerView.Adapter<AdapterSafa.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
 
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(viewGroup.context).inflate(R.layout.list_location, viewGroup, false)
+        return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val (namelokasi, address, poster,deskripsi ) = listSafa [position]
+        holder.dataname.text = namelokasi
+        holder.dataaddress.text = address
+        holder.dataposter.setImageResource(poster)
+        holder.datadeskripsi.text = deskripsi
+
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listSafa [holder.adapterPosition])
+        }
+
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = listSafa.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var dataname: TextView = itemView.findViewById(R.id.namelokasi)
+        var dataaddress: TextView = itemView.findViewById(R.id.addresslokasi)
+        var dataposter: ImageView = itemView.findViewById(R.id.posterlokasi)
+        var datadeskripsi: TextView = itemView.findViewById(R.id.deskripsilokasi)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: DataSafa)
     }
 }
